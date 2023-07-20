eaf46a57c9121067074fd3d14e1021cc81b25f23
normalnine <codejamgg@gmail.com>
2023-07-20 10:39:54 +0900

    230720_KDH_ClearTimer

--- Assets/KDH/Scripts/KDH_Clear.cs
+++ Assets/KDH/Scripts/KDH_Clear.cs
@@ -1,15 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Video;
 
 public class KDH_Clear : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
+    public GameObject clearTimer;
 
     void Start()
     {
-        
-       videoPlayer.Play();
+        videoPlayer.Play();
+
+        StartCoroutine(TimerSpawn());
+    }
+
+    IEnumerator TimerSpawn()
+    {
+        yield return new WaitForSecondsRealtime(4f);
+        clearTimer.GetComponent<Text>().enabled = true;
+
+        yield return new WaitForSecondsRealtime(5f);
+        clearTimer.GetComponent<Text>().enabled = false;
+
     }
 }
